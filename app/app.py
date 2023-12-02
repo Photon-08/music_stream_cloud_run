98a9b8582271867df6fb9ba8f7fdc411996711f5
INDRANIL BHATTACHARYYA <125260412+Photon-08@users.noreply.github.com>
2023-12-02 18:37:17 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -32,5 +32,5 @@ app = create_app()
 #app.app_context().push()
 
 from applications.controllers import *
-app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080))) #for gcp
 
